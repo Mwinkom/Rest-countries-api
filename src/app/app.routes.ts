5130265ae85bf60d59843826531f59dcbc14568e
Mildred Naab <mwinkomnaab@gmail.com>
2025-07-06 16:38:37 +0000

    Add routing configuration for country list and details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    loadComponent: () => 
+        import('./components/country-list/country-list.component').then(m => m.CountryListComponent)
+  },
+  { 
+    path: 'country/:code', 
+    loadComponent: () => 
+        import('./components/country-details/country-details.component').then(m => m.CountryDetailsComponent)
+  },
+  { 
+    path: '**', 
+    redirectTo: '' 
+  }
+];
